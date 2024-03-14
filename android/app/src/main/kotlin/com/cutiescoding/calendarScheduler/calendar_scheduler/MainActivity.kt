efac2a2a88beab977c17be54f167a8e70a27e39b
roy <garam103@naver.com>
2024-03-14 02:35:53 -0700

    load to table calendar

---
+++ android/app/src/main/kotlin/com/cutiescoding/calendarScheduler/calendar_scheduler/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.cutiescoding.calendarScheduler.calendar_scheduler
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:calendar_scheduler/screen/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'NotoSans',
+      ),
+      home: HomeScreen(),
+    )
+  );
+}
