fc520fe602dd35eef052757ae5b226476ff150f7
roy <garam103@naver.com>
2024-03-31 23:16:12 -0700

    todayBanner

--- lib/component/calendar.dart
+++ lib/component/calendar.dart
@@ -3,17 +3,19 @@ import 'package:table_calendar/table_calendar.dart';
 
 import '../const/colors.dart';
 
-class Calendar extends StatefulWidget {
-  const Calendar({Key? key}) : super(key: key);
+class Calendar extends StatelessWidget {
+  final DateTime? selectedDay;
+  final DateTime focusedDay;
+  final OnDaySelected? onDaySelected;
 
-  @override
-  State<Calendar> createState() => _CalendarState();
-}
+  const Calendar(
+      {required this.selectedDay,
+      required this.focusedDay,
+      required this.onDaySelected,
+      Key? key})
+      : super(key: key);
 
-class _CalendarState extends State<Calendar> {
-  DateTime? selectedDay;
-  DateTime focusedDay = DateTime.now();
-
+  //외부에서 받을거라 Calendar로 올려줌
   @override
   Widget build(BuildContext context) {
     final defaultBoxDeco = BoxDecoration(
@@ -32,50 +34,40 @@ class _CalendarState extends State<Calendar> {
       firstDay: DateTime(1880),
       lastDay: DateTime(3000),
       headerStyle: HeaderStyle(
-          formatButtonVisible: false,
-          titleCentered: true,
-          titleTextStyle: TextStyle(
-            fontWeight: FontWeight.w700,
-            fontSize: 16.0,
-          ),
+        formatButtonVisible: false,
+        titleCentered: true,
+        titleTextStyle: TextStyle(
+          fontWeight: FontWeight.w700,
+          fontSize: 16.0,
+        ),
       ),
       calendarStyle: CalendarStyle(
-        //오늘 날짜 표기
-        isTodayHighlighted: false,
-        defaultDecoration: defaultBoxDeco,
-        weekendDecoration: defaultBoxDeco,
-        selectedDecoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(6.0),
-          border: Border.all(
+          //오늘 날짜 표기
+          isTodayHighlighted: false,
+          defaultDecoration: defaultBoxDeco,
+          weekendDecoration: defaultBoxDeco,
+          selectedDecoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(6.0),
+              border: Border.all(
+                color: PRIMARY_COLOR,
+                width: 1.0,
+              )),
+          outsideDecoration: BoxDecoration(shape: BoxShape.rectangle),
+          defaultTextStyle: defaultTextStyle,
+          weekendTextStyle: defaultTextStyle,
+          selectedTextStyle: defaultTextStyle.copyWith(
             color: PRIMARY_COLOR,
-            width: 1.0,
-          )
-        ),
-        outsideDecoration: BoxDecoration(
-          shape: BoxShape.rectangle
-        ),
-        defaultTextStyle: defaultTextStyle,
-        weekendTextStyle: defaultTextStyle,
-        selectedTextStyle: defaultTextStyle.copyWith(
-          color: PRIMARY_COLOR,
-        )
-      ),
-      onDaySelected: (DateTime selectedDay, DateTime focusedDay) {
-        print(selectedDay);
-        setState(() {
-          this.selectedDay = selectedDay;
-          this.focusedDay = selectedDay;
-        });
-      },
+          )),
+      onDaySelected: onDaySelected,
       selectedDayPredicate: (DateTime date) {
-        if(selectedDay == null) {
+        if (selectedDay == null) {
           return false;
         }
 
         return date.year == selectedDay!.year &&
-        date.month == selectedDay!.month &&
-        date.day == selectedDay!.day;
+            date.month == selectedDay!.month &&
+            date.day == selectedDay!.day;
       },
     );
   }
