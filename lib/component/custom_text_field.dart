6b65efedce9df521b2d078e08fc7e09a040d7e3d
roy <garam103@naver.com>
2024-04-13 01:31:24 -0700

    modify textfield - limit type n expand

--- lib/component/custom_text_field.dart
+++ lib/component/custom_text_field.dart
@@ -1,10 +1,13 @@
 import 'package:calendar_scheduler/const/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class CustomTextField extends StatelessWidget {
   final String label;
+  //true - 시간(time) / false - 내용(content)
+  final bool isTime;
 
-  const CustomTextField({required this.label, Key? key}) : super(key: key);
+  const CustomTextField({required this.label, required this.isTime, Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -14,15 +17,27 @@ class CustomTextField extends StatelessWidget {
         Text(label,
             style:
                 TextStyle(color: PRIMARY_COLOR, fontWeight: FontWeight.w600)),
-        TextField(
-          cursorColor: Colors.grey,
-          decoration: InputDecoration(
-            border: InputBorder.none,
-            filled: true,
-            fillColor: Colors.grey[300],
-          ),
-        ),
+        if(isTime) renderTextField(),
+        if(!isTime) Expanded(child: renderTextField(),),
       ],
+    );
+  }
+
+  Widget renderTextField(){
+    return TextField(
+      cursorColor: Colors.grey,
+      maxLines: isTime? 1 : null,
+      expands: !isTime, // = isTime ? false : true,
+      keyboardType: isTime ? TextInputType.number : TextInputType.multiline,
+      //타입 제한하기(여기에서는 컴퓨터 자판을 이용해도 숫자만 적힌다)
+      inputFormatters: isTime ? [
+        FilteringTextInputFormatter.digitsOnly
+      ] : [],
+      decoration: InputDecoration(
+        border: InputBorder.none,
+        filled: true,
+        fillColor: Colors.grey[300],
+      ),
     );
   }
 }
--- lib/component/schedule_bottom_sheet.dart
+++ lib/component/schedule_bottom_sheet.dart
@@ -9,30 +9,38 @@ class ScheduleBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     final bottomInset = MediaQuery.of(context).viewInsets.bottom;
 
-    return Container(
-      height: MediaQuery.of(context).size.height / 2 + bottomInset,
-      color: Colors.white,
-      child: Padding(
-        padding: EdgeInsets.only(bottom: bottomInset),
-        child: Padding(
-          padding: const EdgeInsets.only(left: 8.0, right: 8.0, top: 16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              _Time(),
-              SizedBox(
-                height: 16.0,
+    return GestureDetector(
+      //아무 곳이나 누르면 키보드 닫히게(내려가게) 하기 위해 gestureDetector 사용.
+      onTap: () {
+        FocusScope.of(context).requestFocus((FocusNode()));
+      },
+      child: SafeArea(
+        child: Container(
+          height: MediaQuery.of(context).size.height / 2 + bottomInset,
+          color: Colors.white,
+          child: Padding(
+            padding: EdgeInsets.only(bottom: bottomInset),
+            child: Padding(
+              padding: const EdgeInsets.only(left: 8.0, right: 8.0, top: 16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  _Time(),
+                  SizedBox(
+                    height: 16.0,
+                  ),
+                  _Content(),
+                  SizedBox(
+                    height: 16.0,
+                  ),
+                  _ColorPicker(),
+                  SizedBox(
+                    height: 8.0,
+                  ),
+                  _SaveButton(),
+                ],
               ),
-              _Content(),
-              SizedBox(
-                height: 16.0,
-              ),
-              _ColorPicker(),
-              SizedBox(
-                height: 8.0,
-              ),
-              _SaveButton(),
-            ],
+            ),
           ),
         ),
       ),
@@ -50,6 +58,7 @@ class _Time extends StatelessWidget {
         Expanded(
             child: CustomTextField(
           label: '시작 시간',
+          isTime: true,
         )),
         SizedBox(
           width: 16.0,
@@ -57,6 +66,7 @@ class _Time extends StatelessWidget {
         Expanded(
             child: CustomTextField(
           label: '마감 시간',
+          isTime: true,
         )),
       ],
     );
@@ -68,7 +78,7 @@ class _Content extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CustomTextField(label: '내용');
+    return Expanded(child: CustomTextField(label: '내용', isTime: false,));
   }
 }
 
